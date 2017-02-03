56f197a3839611ee03835e6d8cb357b1263b8236
facuarmo <facumo.fm@gmail.com>
2017-02-02 21:57:54 -0300

    Initialize repo (first release)

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# android_scriptmaker
-A recovery script maker for already installed ROMs.
+# Android Scriptmaker
+
+**Android Scriptmaker** is a little software piece which is able to make the required lines to set the right permissions in a recovery script.
+
+**This script is COMPLETELY compatible with the Unix Shell without any previous modifications.**
+
+Required installed software: tee, grep, wc and cat. *The script will test if your device lacks support of any of them.*
+
+## Usage
+
+* Download the script in RAW format and name it **android_scriptmaker.sh**. Move the file to the root of your internal memory (/sdcard).
+
+* Open a terminal app in your device (or run *adb shell* from your computer).
+
+* Type *cd /* . This step is completely optional, the script detects where you're automatically.
+
+* Type *sh /sdcard/android_scriptmaker*
+
+* Follow the steps described by the script (it has a really verbose output).
+
+* Optionally, delete all permission-related lines in the original updater-script, as long as this scripts generates again all of them one by one.
+
+* Once completed, you can transfer the file **add-this-updater-script** to your computer and append the lines as required.
+
+## Side notes
+
+This is the first project that I've ever done with a point-of-view in production systems. That way, it has been completely tested that it's unable to damage any of the software or hardware parts of your device. Anyways, I won't take any responsability on what could happen if you run it in your device and something fails, you have full access to the source code, take advantage of that before you point your finger at me.
---
+++ android_scriptmaker.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+####################################################################
+## ===> Script author: Facundo Montero (facumo.fm@gmail.com) <=== ##
+####################################################################
+# Workaround for find listing format.
+CURFILE=2
+# Reset (if setted) RUNSCRIPT variable.
+RUNSCRIPT=0
+# Delete existing file (if any) and suppress output (if not existing).
+rm -f /sdcard/add-this-updater-script
+rm -f /sdcard/tempflist
+# By default, this won't be a forced run.
+FORCEDRUN=0
+if [ $PWD != "/" ]
+then
+ WASNTINROOT=1
+ STARTPATH=$PWD
+ echo "Whoops! You're currently in $PWD, but you should be running this script from the root of your device. Anyways, don't be scared, I'll take you to the root of your device by myself :D."
+ cd /
+fi
+echo "##############"
+echo "Welcome, this script will make a appendeable file for your already installed ROM (used for modding)"
+echo "##############"
+echo "Please type 'yes' (without quotes) to continue: "
+read RUNSCRIPT
+if [ "$(id -u)" -ne 0 ]
+then
+ echo "WARNING: The script has detected that you're running it from a normal user (or at least not root). This might cause permissions problems while accessing to the required files. Are you still sure that you want to continue? Type 'yes' to continue: "
+ read RUNSCRIPT
+ if [ $RUNSCRIPT == 'yes' ]
+ then
+  FORCEDRUN=1
+ fi
+fi
+if [ $RUNSCRIPT == 'yes' ]
+ then
+ echo "Okay! We're going to do some test, just to make sure your device supports: tee, grep, wc and cat."
+ echo "##############"
+ echo "Testing 'tee'"
+ echo "##############"
+ echo "test" | tee /sdcard/testtee
+ if [ $? -eq 0 ]
+  then
+  echo "'tee' is working :-)"
+ else
+  echo "Nope, no 'tee'. Please install it and try again."
+  rm -f /sdcard/testee
+  exit 1
+ fi
+ echo "test" | grep "test"
+ if [ $? -eq 0 ]
+  then
+  echo "'grep' is working :-)"
+ else
+  echo "Nope, no 'grep'. Please install it and try again."
+  exit 1
+ fi
+ echo "test" | wc
+ if [ $? -eq 0 ]
+  then
+  echo "'wc' is working :-)"
+ else
+  echo "Nope, no 'wc'. Please install it and try again."
+  exit 1
+ fi
+ cat /sdcard/testtee
+ if [ $? -eq 0 ]
+  then
+  echo "'cat' is working :-)"
+ else
+  echo "Nope, no 'cat'. Please install it and try again."
+  rm -f /sdcard/testtee
+  exit 1
+ fi
+ echo "test" | sed 's/test//'
+ if [ $? -eq 0 ]
+  then
+  echo "'sed' is working :-)"
+ else
+  echo "Nope, no 'sed'. Please install it and try again."
+  exit 1
+ fi
+ find /sdcard > /dev/null 2>&1
+ if [ $? -eq 0 ]
+ then
+  echo "'find' is working :-)"
+ else
+  echo "Nope, no 'find'. Please install it and try again."
+ fi
+fi
+if [ $RUNSCRIPT == 'yes' ]
+ then
+ FILES=./system/*
+ FILEAM=`find $FILES | wc -l`
+ FILEAMCUR=0
+ printf "\n"
+ for d in $(find $FILES | tee /sdcard/android_scriptmaker.log)
+ do
+  FILEAMCUR=$(($FILEAMCUR + 1))
+  CURFILE=`printf "$d" | sed 's-\.--'`
+  PERMS=`stat -c '%A %a %n' $d | cut -d " " -f2`
+  printf "set_perm (1000, 1000, 0$PERMS, \"$CURFILE\");\n" >> /sdcard/add-this-updater-script
+  printf "Processing: ($FILEAMCUR / $FILEAM)\r"
+ done
+ printf "\n"
+ if [ $FORCEDRUN = 1 ]
+ then
+  cat /sdcard/android_scriptmaker.log | grep "Permission denied"
+  if [ $? = 0 ]
+  then
+   echo "Hey! I saw you were running in a forced run, this meaning that you weren't having root permissions while running the script. Also, I saw that, as I warned before, you have had many 'Permission denied' errors. My best advice is for you to try running the script again from a recovery with an integrated terminal such as TWRP (Team Win Recovery Project). The best wishes for you :-)."
+  fi
+ else
+  echo "All done. Enjoy your script ;)."
+ fi
+ if [ $WASNTINROOT = 1 ]
+ then
+  echo "I remember you weren't in the root of your device and I took you by myself here. Bringing you back to home ;)."
+  cd $STARTPATH
+ fi
+else
+ echo "Okie! See you ;)"
+ exit 0
+fi
