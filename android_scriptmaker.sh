1a504347be7665cc154434c3363f2356520738f5
facuarmo <facumo.fm@gmail.com>
2017-02-06 01:47:44 -0300

    Comment everything

--- android_scriptmaker.sh
+++ android_scriptmaker.sh
@@ -13,17 +13,22 @@ rm -f /sdcard/tempflist
 FORCEDRUN=0
 # By default, we should be in root.
 WASNTINROOT=0
+# Test if the script is being run from an Android deice or something else (list root and check if 'sdcard' exists).
 ls / | grep "sdcard"
 if [ $? -ne 0 ]
 then
  echo "Hey! This isn't an Android device. This script only works on Android devices!"
  exit 0
 fi
+# Check if the script is running from the root of the device (right location) or from another location.
 if [ $PWD != "/" ]
 then
+ # Take in mind we weren't in root.
  WASNTINROOT=1
+ # Save initial path.
  STARTPATH=$PWD
  echo "Whoops! You're currently in $PWD, but you should be running this script from the root of your device. Anyways, don't be scared, I'll take you to the root of your device by myself :D."
+ # Take the user to the root.
  cd /
 fi
 echo "##############"
@@ -49,6 +54,8 @@ if [ "$RUNSCRIPT" = 'yes' ]
  echo "##############"
  echo "Testing 'grep'"
  echo "##############"
+ # Every 'command > /dev/null 2>&1' means that stdout and stderr are suppressed. We only want to save the exit status (if errors were found or not).
+ # We exit with a "general error" status (1) if a required binary isn't present.
  echo "test" | grep "test" > /dev/null 2>&1
  if [ $? -eq 0 ]
   then
@@ -105,22 +112,34 @@ if [ "$RUNSCRIPT" = 'yes' ]
 fi
 if [ "$RUNSCRIPT" = 'yes' ]
  then
+ # Set beggining of the search to 'system' partition and everything after.
  FILES=./system/*
+ # Get the full list of files and count it as lines to know the amount of existing files (this will be used for the counter during the process).
  FILEAM=`find $FILES | wc -l`
+ # Reset current file to zero (just in case).
  FILEAMCUR=0
  printf "\n"
+ # Scan the path saved in $FILES.
+ # If any errors occur they'll be saved to a log at the sdcard.
  for d in $(find $FILES 2> /sdcard/android_scriptmaker.log)
  do
   FILEAMCUR=$(($FILEAMCUR + 1))
+  # Process the line to get rid of the dots. Escaped character as it's taken as a comand if not escaped.
+  # In the script, this will be a partition, not exactly a path that we can see from a current dir.
   CURFILE=`printf "$d" | sed 's-\.--'`
+  # Get the current file from the looped variable and get it's permissions.
   PERMS=`stat -c '%A %a %n' $d | cut -d " " -f2`
+  # Save data to the script.
   printf "set_perm (1000, 1000, 0$PERMS, \"$CURFILE\");\n" >> /sdcard/add-this-updater-script
+  # Show process (don't generate new lines, just update the last line).
   printf "Processing: ($FILEAMCUR / $FILEAM)\r"
  done
  printf "\n"
+ # If the user accepted to use the script anyways and wasn't root, try checking if any permissions error has been found.
  if [ $FORCEDRUN -eq 1 ]
  then
   cat /sdcard/android_scriptmaker.log | grep "Permission denied"
+  # If found (grep exit status 0, no errors) tell the user to try running the script from recovery.
   if [ $? = 0 ]
   then
    echo "Hey! I saw you were running in a forced run, this meaning that you weren't having root permissions while running the script. Also, I saw that, as I warned before, you have had many 'Permission denied' errors. My best advice is for you to try running the script again from a recovery with an integrated terminal such as TWRP (Team Win Recovery Project). The best wishes for you :-)."
@@ -128,6 +147,7 @@ if [ "$RUNSCRIPT" = 'yes' ]
  else
   echo "All done. Enjoy your script ;)."
  fi
+ # If this variable is set to 1, take the user back to the starting path before exitting.
  if [ $WASNTINROOT -eq 1 ]
  then
   echo "I remember you weren't in the root of your device and I took you by myself here. Bringing you back to home ;)."
